a0b1ae017d3990cbfa56e94fda0a15966e0a3b8d
Daniel Levy Moreno <daniellevymoreno@gmail.com>
2021-04-25 10:53:52 +0200

    Removed .envrc, this sucks, find something else.

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-with (import <nixpkgs> {});
+{ ghc }: with (import <nixpkgs> {});
 
 # FIXME https://stackoverflow.com/questions/27713707/nix-shell-how-to-specify-a-custom-environment-variable
 # Use `XMONAD_CONFIG_HOME` and etc... https://wiki.archlinux.org/index.php/XDG_Base_Directory.
